40393a2ffa06ee9d2b8f9bcc2f8e6b385534a4ef
Emin Eker <emineker@bil.omu.edu.tr>
2013-01-08 01:27:55 +0200

    yeni labirent nesnesi üzerinde çalış

--- solver.rb
+++ solver.rb
@@ -4,69 +4,64 @@ require 'theseus/orthogonal_maze'
 require 'theseus/solvers/base'
 require 'fileutils'
 
-# A* algoritması kullanarak hazırlanmış bi uygulama.
-class Astar < Theseus::Solvers::Base
+# bu nesne, her bir hücrenin kontrolü sırasında ilişki geçmişini kontrol eder
+# ve hesaplanmış maliyetler doğrultusunda kontrol edeceği bir sonraki noktayı seçer
+#
+# nadiren bu sınıfı kullanmamız gerekmesine rağmen,
+# eğer çözüm algoritması görselleştirmek istenirse
+# açık küme bağlantıları kullanılarak gerçekleştirilebilir
+class Node
+  # düğüm nesnelerinin karşılaştırılabilmesi için kullan
+  include Comparable
 
-  # bu nesne, her bir hücrenin kontrolü sırasında ilişki geçmişini kontrol eder
-  # ve hesaplanmış maliyetler doğrultusunda kontrol edeceği bir sonraki noktayı seçer
-  #
-  # nadiren bu sınıfı kullanmamız gerekmesine rağmen,
-  # eğer çözüm algoritması görselleştirmek istenirse
-  # açık küme baülantıları kullanılarak gerçekleştirilebilir
-  class Node
-    # düğüm nesnelerinin karşılaştırılabilmesi bu nesne gerekli
-    include Comparable
+  # labirentin bu düğüm ile ilgili olan noktası
+  attr_accessor :point
 
-    # labirentin bu düğüm ile ilgili olan noktası
-    attr_accessor :point
+  # düğüm birincil düzlemde ise yanlış, alt düzlemde ise doğru olsun
+  attr_accessor :under
 
-    # Düğüm birincil düzlemde ile yanlış, alt düzlemde ise doğru olsun
-    attr_accessor :under
+  # bu düğümün yol maliyeti: başlangıçtan itibaren bu düğüme olan mesafe
+  # (labirent üzerinden yapılan hesap)
+  attr_accessor :path_cost
 
-    # bu düğümün yol maliyeti: başlangıçtan itibaren bu düğüme olan mesafe
-    # (labirent üzerinden yapılan hesap)
-    attr_accessor :path_cost
+  # çıkış bu düğümden tahminen (iyimser bir tahminle) ne kadar ilerde
+  attr_accessor :estimate
 
-    # çıkış bu düğümden tahminen (iyimser bir tahminle) ne kadar ilerde
-    attr_accessor :estimate
+  # bu düğüm ile ilişkili toplam maliyet (path_cost + tahmini)
+  attr_accessor :cost
 
-    # bu düğüm ile ilişkili toplam maliyet (path_cost + tahmini)
-    attr_accessor :cost
+  # bağlantılı listesinde bu düğüme ait olan bir sonraki düğüm
+  attr_accessor :next
 
-    # bağlantılı listesinde bu düğüme ait olan bir sonraki düğüm
-    attr_accessor :next
+  # başlangıç noktasından, bu düğüme giden noktaları dizisi
+  attr_reader :history
 
-    # başlangıç noktasından, bu düğümün giden noktaları dizisi
-    attr_reader :history
-
-    def initialize(point, under, path_cost, estimate, history)
-      @point, @under, @path_cost, @estimate = point, under, path_cost, estimate
-      @history = history
-      @cost = path_cost + estimate
-    end
-
-    def <=>(node)
-      cost <=> node.cost
-    end
+  def initialize(point, under, path_cost, estimate, history)
+    @point, @under, @path_cost, @estimate, @history = point, under, path_cost, estimate, history
+    @cost = path_cost + estimate
   end
 
+  def <=>(node)
+    cost <=> node.cost
+  end
+end
+
+
+class Solver < Theseus::Solvers::Base
   # A* algoritması tarafından kullanılan düğüm örnekleri
   # ve bağlantı listelerinin dikkate alınması gerekmektedir
   # bu liste her zaman en olasıdan başlayarak sıralanır
   attr_reader :open
 
-  def initialize(maze, a=maze.start, b=maze.finish)
+  def initialize(maze)
     super
+    @a, @b = maze.start, maze.finish
     @open = Node.new(@a, false, 0, estimate(@a), [])
     @visits = Array.new(@maze.height) { Array.new(@maze.width, 0) }
   end
 
-  def current_solution
-    @open.history + [@open.point]
-  end
-
   def step
-    return false unless @open
+    return unless @open
 
     current = @open
 
@@ -82,29 +77,28 @@ class Astar < Theseus::Solvers::Base
 
       directions = @maze.potential_exits_at(current.point[0], current.point[1])
       directions.each do |dir|
-        try = current.under ? (dir << Theseus::Maze::UNDER_SHIFT) : dir
+        try = current.under ? (dir << UNDER_SHIFT) : dir
         if cell & try != 0
-          point = move(current.point, dir)
+          point = [current.point[0] + @maze.dx(dir), current.point[1] + @maze.dy(dir)]
           next unless @maze.valid?(point[0], point[1])
-          under = ((@maze[point[0], point[1]] >> Theseus::Maze::UNDER_SHIFT) & @maze.opposite(dir) != 0)
+          under = ((@maze[point[0], point[1]] >> UNDER_SHIFT) & @maze.opposite(dir) != 0)
           add_node(point, under, current.path_cost+1, current.history + [current.point])
         end
       end
     end
-
-    return current
   end
 
   private
 
-  def estimate(pt)
-    Math.sqrt((@b[0] - pt[0])**2 + (@b[1] - pt[1])**2)
+  # tahmini yol hesabı
+  def estimate(point)
+    Math.sqrt((@b[0] - point[0])**2 + (@b[1] - point[1])**2)
   end
 
-  def add_node(pt, under, path_cost, history)
-    return if @visits[pt[1]][pt[0]] & (under ? 2 : 1) != 0
+  def add_node(point, under, path_cost, history)
+    return if @visits[point[1]][point[0]] & (under ? 2 : 1) != 0
 
-    node = Node.new(pt, under, path_cost, estimate(pt), history)
+    node = Node.new(point, under, path_cost, estimate(point), history)
 
     if @open
       p, n = nil, @open
@@ -131,12 +125,16 @@ class Astar < Theseus::Solvers::Base
     end
   end
 
-  def move(pt, direction)
-    [pt[0] + @maze.dx(direction), pt[1] + @maze.dy(direction)]
-  end
 end
 
+# adımların resimlerinin tutulduğu dizin
 STEPDIR = "steps/"
+
+# bit maskesi boyutu
+UNDER_SHIFT = 8
+
+# çözüm adımları
+step = 0
 
 # adımlama dizini yoksa oluştur
 Dir.mkdir(STEPDIR) unless File.exist?(STEPDIR)
@@ -144,21 +142,20 @@ Dir.mkdir(STEPDIR) unless File.exist?(STEPDIR)
 # dizindeki eski resimleri temizle
 FileUtils.rm_rf("#{STEPDIR}.")
 
-# herhangi bir çoklu bağlantılı labirent oluştur
+# theseus paketi ile çoklu bağlantılı labirent oluştur
 maze = Theseus::OrthogonalMaze.new(width: 10, height: 10)
 maze.generate!
 
-# A* arama algoritması için yeni bir çözüm nesnesi oluştur
-# solver = Astar.new(maze, maze.start, maze.finish)
-solver = Astar.new(maze)
+puts maze
+print "\nDevam etmek için bir tuşa basın..."
+STDIN.gets.chomp
 
-puts "lebirent çözülüyor..."
 
-step = 0
+# bir çözüm nesnesi oluştur
+solver = Solver.new(maze)
 
-# daha önce kullanılmış ve artık bu yoldan biryere ulaşılamayacağı
-# anlaşıldığında bu alanları gri ile renklendir
-# eskimiş yollar
+# eskimiş yollar: daha önce kullanılmış ve artık bu yoldan biryere
+# ulaşılamayacağı anlaşıldığında gri ile renklendir
 stale_paths = maze.new_path(color: 0x9f9f9fff)
 
 while solver.step
